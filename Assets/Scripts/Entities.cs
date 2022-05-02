899875348a81c110cac916c82e8284b7b2e7a48f
Darelund <edlund.vidar@gmail.com>
2022-05-02 19:43:06 +0200

    Change2
    Change2

--- Assets/Scripts/Entities.cs
+++ Assets/Scripts/Entities.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EntitiesState
+{
+    idle,
+    walk,
+    attack,
+    stagger
+}
+
 public class Entities : MonoBehaviour
 {
+    public EntitiesState currentState;
     public int health;
     public string entitieName;
     public int baseAttack;
--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Log : Entities
 {
+    private Rigidbody2D logRigidbody;
     public Transform target;
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentState = EntitiesState.idle;
+        logRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         CheckDistance();
     }
@@ -27,7 +32,18 @@ public class Log : Entities
         // if chase radius is more or equal to the distance between target and object with this script then run if
         if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, movespeed * Time.deltaTime);
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, movespeed * Time.deltaTime);
+
+            logRigidbody.MovePosition(temp);
+            ChangeState(EntitiesState.walk);
+        }
+    }
+
+    private void ChangeState(EntitiesState newState)
+    {
+        if(currentState != newState)
+        {
+            currentState = newState;
         }
     }
 }
