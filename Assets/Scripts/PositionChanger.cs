f385ab3355cee2db030df9285f5f86d3df5678b2
Darelund <edlund.vidar@gmail.com>
2022-05-08 22:39:55 +0200

    Changed portal script
    Renamed portal script to position changer

--- Assets/Scripts/PositionChanger.cs
+++ Assets/Scripts/PositionChanger.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+public class PositionChanger : MonoBehaviour
 {
     public Vector3 spawnPosition;
 
     private void Awake()
     {
-       
+
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player") && !other.isTrigger)
+        if (other.CompareTag("Player") && !other.isTrigger)
         {
             other.transform.position = spawnPosition;
-            
+
         }
     }
 }
