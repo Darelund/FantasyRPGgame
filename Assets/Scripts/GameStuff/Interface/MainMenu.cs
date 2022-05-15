1c4ad5b84dadfd4b06a3021be40491b391bdf370
Darelund <edlund.vidar@gmail.com>
2022-05-15 21:25:43 +0200

    Cutscene added
    Working cutscene

--- Assets/Scripts/GameStuff/Interface/MainMenu.cs
+++ Assets/Scripts/GameStuff/Interface/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
 
     public void NewGame()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("OpeningCutscene");
     }
 
     // Need to be added
