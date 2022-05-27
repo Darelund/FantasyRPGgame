54fd4b764bdde567ec81a5b3b2db88655b709dea
Darelund <edlund.vidar@gmail.com>
2022-05-27 11:49:01 +0200

    ggjgg

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    public string[] array;
-
+    // A public variable with the type float and name "speed" with the value of 5f
+    public float speed = 5f;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-      
+        
     }
     // Update is called once per frame
-    void Update()
+   public  void Update()
     {
-      foreach(string i in array)
-        {
-            Debug.Log(i);        
-        }
+        // Will find the gameobject that is attached to this script, find its transform and then it will translate its x, y and z axis by some values
+        transform.Translate(speed, 0, 0);
     } 
 }
