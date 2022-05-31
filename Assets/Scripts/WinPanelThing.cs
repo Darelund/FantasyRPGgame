f7769e0af454d1e00515fdd171931c28a91e9442
Darelund <edlund.vidar@gmail.com>
2022-05-31 17:59:58 +0200

    dhdgggdg

---
+++ Assets/Scripts/WinPanelThing.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinPanelThing : MonoBehaviour
+{
+    private PlayerMovement player;
+    private FloatValue value;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChallengeGod()
+    {
+
+
+        SceneManager.LoadScene("Godbattle");
+
+    }
+
+    // Need to be added
+    public void LoadGame()
+    {
+
+    }
+
+    // Need to be added
+    public void Options()
+    {
+
+    }
+
+    public void GameFinished()
+    {
+        SceneManager.LoadScene("GameFinished");
+    }
+}
